9eb9da5c25471d3341c9e9cf9355d75a0c3595dd
Paulo Santana <paulosantana.sa@gmail.com>
2022-03-07 20:48:47 -0300

    added script to run an interactive link

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+const linkSocialMedia = {
+    github: "paulosantana95",
+    linkedin: "in/paulosantana95",
+    instagram: "paulosantana.sa",
+    facebook: "p.santana95"
+}
+
+
+function changeSocialMediaLinks() {
+
+    for (let li of socialLinks.children) {
+        const social = li.getAttribute('class')
+
+        li.children[0].href = `https://${social}.com/${linkSocialMedia[social]}`
+    }
+}
+
+changeSocialMediaLinks();
